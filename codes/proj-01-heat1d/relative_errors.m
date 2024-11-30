fdd5194b59e7dbd606fb466fff805c7d7d835adf
Renli12112321 <12112321@mail.sustech.edu.cn>
2024-11-30 21:22:28 +0800

    changed the mesh,added the IEN and quadratrue rule

--- codes/proj-01-heat1d/relative_errors.m
+++ codes/proj-01-heat1d/relative_errors.m
@@ -1,14 +1,27 @@
 clear all; clc; clf; % clean the memory, screen, and figure
 
 % Problem definition
-f = @(x) -20*x.^3; % f(x) is the source
+f = @(x) -20*x.^3; % f(x)
 g = 1.0;           % u    = g  at x = 1
 h = 0.0;           % -u,x = h  at x = 0
 
 % Setup the mesh
 pp   = 2;              % polynomial degree
 n_en = pp + 1;         % number of element or local nodes
-n_el = 2;              % number of elements
+for n_el = 2:2:16      % mesh with element number from 2 to 16
+    hh = 1 / n_el /n_en;
+    x_coor = 0 : hh : 1;
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
+
+%Setup the IEN
+IEN = zeros(n_el, n_en);
+for ee = 1 : n_el
+  for aa = 1 : n_en
+    IEN(ee, aa) = (ee - 1) * pp + aa;
+  end
+end
+
+% Setup the quadrature rule
 n_int = 10;
+[xi, weight] = Gauss(n_int, -1, 1);
