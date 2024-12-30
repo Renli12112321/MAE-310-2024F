b2d1f4fbd7d86ae2699fc7a83a8d762669731c3b
Renli12112321 <12112321@mail.sustech.edu.cn>
2024-12-31 02:02:39 +0800

    复制了1d时的error代码

--- codes/tri/conv.m
+++ codes/tri/conv.m
@@ -15,143 +15,187 @@ n_int_eta = 3;
 n_int     = n_int_xi * n_int_eta;
 [xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
 
-% mesh generation
-n_en   = 3;                   % number of nodes in an triangle element
-n_el_x = 60;                  % number of elements in x-dir
-n_el_y = 60;                  % number of elements in y-dir
-n_el   = n_el_x * n_el_y * 2; % total number of elements
+% mesh size loop
+logeL2 = zeros(8,1);
+logeH1 = zeros(8,1);
+logh   = zeros(8,1);
+for n_me = 60:20:200
 
-n_np_x = n_el_x + 1;      % number of nodal points in x-dir
-n_np_y = n_el_y + 1;      % number of nodal points in y-dir
-n_np   = n_np_x * n_np_y; % total number of nodal points
+    % mesh generation
+    n_en   = 3;                   % number of nodes in an triangle element
+    n_el_x = n_me;                  % number of elements in x-dir
+    n_el_y = n_me;                  % number of elements in y-dir
+    n_el   = n_el_x * n_el_y * 2; % total number of elements
 
-x_coor = zeros(n_np, 1);
-y_coor = x_coor;
+    n_np_x = n_el_x + 1;      % number of nodal points in x-dir
+    n_np_y = n_el_y + 1;      % number of nodal points in y-dir
+    n_np   = n_np_x * n_np_y; % total number of nodal points
 
-hx = 1.0 / n_el_x;        % mesh size in x-dir
-hy = 1.0 / n_el_y;        % mesh size in y-dir
+    x_coor = zeros(n_np, 1);
+    y_coor = x_coor;
 
-% generate the nodal coordinates
-for ny = 1 : n_np_y
-  for nx = 1 : n_np_x
-    index = (ny-1)*n_np_x + nx; % nodal index
-    x_coor(index) = (nx-1) * hx;
-    y_coor(index) = (ny-1) * hy;
-  end
-end
+    hx = 1.0 / n_el_x;        % mesh size in x-dir
+    hy = 1.0 / n_el_y;        % mesh size in y-dir
 
-% IEN array
-IEN = zeros(n_el, n_en);
-for ex = 1 : n_el_x
-  for ey = 1 : n_el_y
-    ee = (ey-1) * n_el_x + ex; % element index
-    IEN(ee*2-1, 1) = (ey-1) * n_np_x + ex;
-    IEN(ee*2-1, 2) = (ey-1) * n_np_x + ex + 1;
-    IEN(ee*2-1, 3) =  ey    * n_np_x + ex;
-    IEN(ee*2, 1)   =  ey    * n_np_x + ex + 1;
-    IEN(ee*2, 2)   =  ey    * n_np_x + ex;
-    IEN(ee*2, 3)   = (ey-1) * n_np_x + ex + 1;
-  end
-end
-
-% ID array
-ID = zeros(n_np,1);
-counter = 0;
-for ny = 2 : n_np_y - 1
-  for nx = 2 : n_np_x - 1
-    index = (ny-1)*n_np_x + nx;
-    counter = counter + 1;
-    ID(index) = counter;  
-  end
-end
-
-n_eq = counter;
-
-LM = ID(IEN);
-
-% allocate the stiffness matrix and load vector
-K = spalloc(n_eq, n_eq, 9 * n_eq);
-F = zeros(n_eq, 1);
-
-% loop over element to assembly the matrix and vector
-for ee = 1 : n_el
-  x_ele = x_coor( IEN(ee, 1:n_en) );
-  y_ele = y_coor( IEN(ee, 1:n_en) );
-  
-  k_ele = zeros(n_en, n_en); % element stiffness matrix
-  f_ele = zeros(n_en, 1);    % element load vector
-  
-  for ll = 1 : n_int
-    x_l = 0.0; y_l = 0.0;
-    dx_dxi = 0.0; dx_deta = 0.0;
-    dy_dxi = 0.0; dy_deta = 0.0;
-    for aa = 1 : n_en
-      x_l = x_l + x_ele(aa) * tri(aa, xi(ll), eta(ll));
-      y_l = y_l + y_ele(aa) * tri(aa, xi(ll), eta(ll));    
-      [Na_xi, Na_eta] = tri_grad(aa, xi(ll), eta(ll));
-      dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
-      dx_deta = dx_deta + x_ele(aa) * Na_eta;
-      dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
-      dy_deta = dy_deta + y_ele(aa) * Na_eta;
-    end
-    
-    detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
-    
-    for aa = 1 : n_en
-      Na = tri(aa, xi(ll), eta(ll));
-      [Na_xi, Na_eta] = tri_grad(aa, xi(ll), eta(ll));
-      Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
-      Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
-      
-      f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;
-      
-      for bb = 1 : n_en
-        Nb = tri(bb, xi(ll), eta(ll));
-        [Nb_xi, Nb_eta] = tri_grad(bb, xi(ll), eta(ll));
-        Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
-        Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
-        
-        k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);
-      end % end of bb loop
-    end % end of aa loop
-  end % end of quadrature loop
- 
-  for aa = 1 : n_en
-    PP = LM(ee, aa);
-    if PP > 0
-      F(PP) = F(PP) + f_ele(aa);
-      
-      for bb = 1 : n_en
-        QQ = LM(ee, bb);
-        if QQ > 0
-          K(PP, QQ) = K(PP, QQ) + k_ele(aa, bb);
-        else
-          % modify F with the boundary data
-          % here we do nothing because the boundary data g is zero or
-          % homogeneous
+    % generate the nodal coordinates
+    for ny = 1 : n_np_y
+        for nx = 1 : n_np_x
+            index = (ny-1)*n_np_x + nx; % nodal index
+            x_coor(index) = (nx-1) * hx;
+            y_coor(index) = (ny-1) * hy;
         end
-      end  
     end
-  end
+
+    % IEN array
+    IEN = zeros(n_el, n_en);
+    for ex = 1 : n_el_x
+        for ey = 1 : n_el_y
+            ee = (ey-1) * n_el_x + ex; % element index
+            IEN(ee*2-1, 1) = (ey-1) * n_np_x + ex;
+            IEN(ee*2-1, 2) = (ey-1) * n_np_x + ex + 1;
+            IEN(ee*2-1, 3) =  ey    * n_np_x + ex;
+            IEN(ee*2, 1)   =  ey    * n_np_x + ex + 1;
+            IEN(ee*2, 2)   =  ey    * n_np_x + ex;
+            IEN(ee*2, 3)   = (ey-1) * n_np_x + ex + 1;
+        end
+    end
+
+    % ID array
+    ID = zeros(n_np,1);
+    counter = 0;
+    for ny = 2 : n_np_y - 1
+        for nx = 2 : n_np_x - 1
+            index = (ny-1)*n_np_x + nx;
+            counter = counter + 1;
+            ID(index) = counter;
+        end
+    end
+
+    n_eq = counter;
+
+    LM = ID(IEN);
+
+    % allocate the stiffness matrix and load vector
+    K = spalloc(n_eq, n_eq, 9 * n_eq);
+    F = zeros(n_eq, 1);
+
+    % loop over element to assembly the matrix and vector
+    for ee = 1 : n_el
+        x_ele = x_coor( IEN(ee, 1:n_en) );
+        y_ele = y_coor( IEN(ee, 1:n_en) );
+
+        k_ele = zeros(n_en, n_en); % element stiffness matrix
+        f_ele = zeros(n_en, 1);    % element load vector
+
+        for ll = 1 : n_int
+            x_l = 0.0; y_l = 0.0;
+            dx_dxi = 0.0; dx_deta = 0.0;
+            dy_dxi = 0.0; dy_deta = 0.0;
+            for aa = 1 : n_en
+                x_l = x_l + x_ele(aa) * tri(aa, xi(ll), eta(ll));
+                y_l = y_l + y_ele(aa) * tri(aa, xi(ll), eta(ll));
+                [Na_xi, Na_eta] = tri_grad(aa, xi(ll), eta(ll));
+                dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
+                dx_deta = dx_deta + x_ele(aa) * Na_eta;
+                dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
+                dy_deta = dy_deta + y_ele(aa) * Na_eta;
+            end
+
+            detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
+
+            for aa = 1 : n_en
+                Na = tri(aa, xi(ll), eta(ll));
+                [Na_xi, Na_eta] = tri_grad(aa, xi(ll), eta(ll));
+                Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
+                Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
+
+                f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;
+
+                for bb = 1 : n_en
+                    Nb = tri(bb, xi(ll), eta(ll));
+                    [Nb_xi, Nb_eta] = tri_grad(bb, xi(ll), eta(ll));
+                    Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
+                    Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
+
+                    k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);
+                end % end of bb loop
+            end % end of aa loop
+        end % end of quadrature loop
+
+        for aa = 1 : n_en
+            PP = LM(ee, aa);
+            if PP > 0
+                F(PP) = F(PP) + f_ele(aa);
+
+                for bb = 1 : n_en
+                    QQ = LM(ee, bb);
+                    if QQ > 0
+                        K(PP, QQ) = K(PP, QQ) + k_ele(aa, bb);
+                    else
+                        % modify F with the boundary data
+                        % here we do nothing because the boundary data g is zero or
+                        % homogeneous
+                    end
+                end
+            end
+        end
+    end
+
+    % solve the stiffness matrix
+    dn = K \ F;
+
+    % insert dn back into the vector for all nodes
+    disp = zeros(n_np, 1);
+
+    for ii = 1 : n_np
+        index = ID(ii);
+        if index > 0
+            disp(ii) = dn(index);
+        else
+            % modify disp with the g data. Here it does nothing because g is zero
+        end
+    end
+
+
+    % calculate the error
+
+    L2_top = 0; L2_bot = 0; H1_top = 0; H1_bot = 0;
+
+    for ee = 1 : n_el
+        x_ele = x_coor( IEN(ee, :) );
+        u_ele = disp( IEN(ee, :) );
+
+        for ll = 1 : n_int
+            x_l = 0.0; uh = 0.0; dx_dxi = 0.0; uh_xi = 0.0;
+            for aa = 1 : n_en
+                x_l    = x_l    + x_ele(aa) * PolyShape(pp, aa, xi(ll), 0);
+                uh     = uh     + u_ele(aa) * PolyShape(pp, aa, xi(ll), 0);
+                dx_dxi = dx_dxi + x_ele(aa) * PolyShape(pp, aa, xi(ll), 1);
+                uh_xi  = uh_xi  + u_ele(aa) * PolyShape(pp, aa, xi(ll), 1);
+            end
+            dxi_dx = 1.0 / dx_dxi;
+
+            L2_top = L2_top + weight(ll) * (uh - ex_u(x_l))^2 * dx_dxi;
+            L2_bot = L2_bot + weight(ll) * ex_u(x_l)^2 * dx_dxi;
+
+            H1_top = H1_top + weight(ll) * ( uh_xi * dxi_dx - ex_du(x_l) )^2 * dx_dxi;
+            H1_bot = H1_bot + weight(ll) * ex_du(x_l)^2 * dx_dxi;
+
+        end
+    end
+
+    L2_top = sqrt(L2_top); L2_bot = sqrt(L2_bot);
+    H1_top = sqrt(H1_top); H1_bot = sqrt(H1_bot);
+
+
+    % 保存该mesh结果到数组
+    logeL2(n_el/2) = log(L2_top / L2_bot);
+    logeH1(n_el/2) = log(H1_top / H1_bot);
+    logh  (n_el/2) = log(hh);
+
 end
-
-% solve the stiffness matrix
-dn = K \ F;
-
-% insert dn back into the vector for all nodes
-disp = zeros(n_np, 1);
-
-for ii = 1 : n_np
-  index = ID(ii);
-  if index > 0
-    disp(ii) = dn(index);
-  else
-    % modify disp with the g data. Here it does nothing because g is zero
-  end
-end
-
 % save the solution vector and number of elements to disp with name
 % HEAT.mat
-save("HEAT", "disp", "n_el_x", "n_el_y");
+%save("HEAT", "disp", "n_el_x", "n_el_y");
 
 % EOF
