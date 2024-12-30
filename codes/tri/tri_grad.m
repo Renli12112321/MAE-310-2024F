ea08584cd0295b5745b2c57a6dfb48f1f5732193
Renli12112321 <12112321@mail.sustech.edu.cn>
2024-12-31 00:09:27 +0800

    改gradient

--- codes/tri/tri_grad.m
+++ codes/tri/tri_grad.m
@@ -1,19 +1,17 @@
-function [val_xi, val_eta] = Quad_grad(aa, xi, eta)
+function [val_xi, val_eta] = tri_grad(aa, xi, eta)
 
 if aa == 1
-    val_xi  = -0.25 * (1-eta);
-    val_eta = -0.25 * (1-xi);
+    val_xi  = -1;
+    val_eta = -1;
 elseif aa == 2
-    val_xi  =  0.25 * (1-eta);
-    val_eta = -0.25 * (1+xi);
+    val_xi  =  1;
+    val_eta =  0;
 elseif aa == 3
-    val_xi  = 0.25 * (1+eta);
-    val_eta = 0.25 * (1+xi);
-elseif aa == 4
-    val_xi  = -0.25 * (1+eta);
-    val_eta =  0.25 * (1-xi);
+    val_xi  =  0;
+    val_eta =  1;
+
 else
-    error('Error: value of a should be 1,2,3, or 4.');
+    error('Error: value of aa should be 1,2,3.');
 end
 
 % EOF
