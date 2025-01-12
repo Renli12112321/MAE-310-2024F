95216274ce2a946dd4a464b42eec2a18818d3a1b
Renli12112321 <12112321@mail.sustech.edu.cn>
2025-01-13 00:48:43 +0800

    vis

--- codes/proj-final/vis_2d_sol.m
+++ codes/proj-final/vis_2d_sol.m
@@ -1,6 +1,6 @@
 clear all; clc; clf;
 
-load("HEAT.mat");
+load("disp.mat");
 
 hh_x = 1.0 / n_el_x;
 hh_y = 1.0 / n_el_y;
@@ -9,7 +9,8 @@ n_np_x = n_el_x + 1;
 n_np_y = n_el_y + 1;
 
 [X, Y] = meshgrid(0 : hh_x : 1, 0 : hh_y : 1);
-Z = reshape(disp, n_np_x, n_np_y)';
+hold on
+Z = reshape(disp(:,1), n_np_x, n_np_y)';
 surf(X, Y, Z);
 
 shading interp
